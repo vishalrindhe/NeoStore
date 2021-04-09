97d62d17a332638121ecf7cb7678685fba0637c4
Vishal Rindhe <rindhevishal32@gmail.com>
2021-04-09 05:45:23 +0530

    ready for friday assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from 'src/assets/services/auth.guard';
+import { AddressComponent } from './pages/address/address.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { ChangePasswordComponent } from './pages/change-password/change-password.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { InvoiceComponent } from './pages/invoice/invoice.component';
 import { LoginComponent } from './pages/login/login.component';
+import { OrderDetailsComponent } from './pages/order-details/order-details.component';
 import { OrderComponent } from './pages/order/order.component';
 import { ProductInfoComponent } from './pages/product-info/product-info.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { RateProductComponent } from './pages/rate-product/rate-product.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 
 const routes: Routes = [
@@ -20,6 +25,11 @@ const routes: Routes = [
   {path:'products' , canActivate : [AuthGuard] , component:ProductsComponent},
   {path:'order' , canActivate : [AuthGuard] , component:OrderComponent},
   {path:'productInfo' , canActivate : [AuthGuard] , component:ProductInfoComponent},
+  {path:'rateproduct' , canActivate : [AuthGuard] , component:RateProductComponent},
+  {path:'address' , canActivate : [AuthGuard] , component:AddressComponent},
+  {path:'changepassword' , canActivate : [AuthGuard] , component:ChangePasswordComponent},
+  {path:'orderdetails' , canActivate : [AuthGuard] , component:OrderDetailsComponent},
+  {path:'invoice' , canActivate : [AuthGuard] , component:InvoiceComponent},
 
 ];
 
